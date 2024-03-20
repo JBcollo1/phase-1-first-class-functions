b51af911fc19b7a8ba5a518d2b1b3c824ec1fb57
jbcollins254@gmail.com <jbcollins254@gmail.com>
2024-03-20 10:46:00 +0300

    completes lab

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function receivesAFunction(spy){
+    spy()
+}
+
+
+function returnsANamedFunction(){
+    function fn(){
+
+    }
+    return fn
+}
+
+function returnsAnAnonymousFunction() {
+    
+    function fn() {
+        
+        }
+  
+    return () => {}
+  }
+ 
+
+
